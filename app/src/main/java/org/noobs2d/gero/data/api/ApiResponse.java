d48b0645bd1bd9700870ce6e13f6ace9e9f1f6dc
Julious Igmen <jcigmen@gmail.com>
2016-11-15 14:26:44 +0800

    Added #isSuccessful()

--- app/src/main/java/org/noobs2d/gero/data/api/ApiResponse.java
+++ app/src/main/java/org/noobs2d/gero/data/api/ApiResponse.java
@@ -6,6 +6,11 @@ import com.google.gson.annotations.SerializedName;
  * @author Julious Igmen
  */
 public abstract class ApiResponse {
+
     @SerializedName("message") String message;
     @SerializedName("status") int status;
+
+    public boolean isSuccessful() {
+        return status == 200 && message != null && !message.isEmpty();
+    }
 }
