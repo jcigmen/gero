5cdddf230454092354cb4c45bc388b6db15743f7
Julious Igmen <jcigmen@gmail.com>
2016-11-15 14:30:58 +0800

    Removed unused imports

--- app/src/main/java/org/noobs2d/gero/data/android/repository/web/update/UpdateAndroidsWebService.java
+++ app/src/main/java/org/noobs2d/gero/data/android/repository/web/update/UpdateAndroidsWebService.java
@@ -2,10 +2,7 @@ package org.noobs2d.gero.data.android.repository.web.update;
 
 import org.noobs2d.gero.data.android.entity.Android;
 
-import java.util.List;
-
 import io.reactivex.Completable;
-import io.reactivex.Observable;
 
 /**
  * @author Julious Igmen
