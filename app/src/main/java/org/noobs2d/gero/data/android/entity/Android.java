4d85cea1c471964c81cfe2ecdaa22a2c15b9234d
Julious Igmen <jcigmen@gmail.com>
2016-11-21 01:33:07 +0800

    Added AutoValue Gson Adapter

--- app/src/main/java/org/noobs2d/gero/data/android/entity/Android.java
+++ app/src/main/java/org/noobs2d/gero/data/android/entity/Android.java
@@ -3,6 +3,8 @@ package org.noobs2d.gero.data.android.entity;
 import android.support.annotation.NonNull;
 
 import com.google.auto.value.AutoValue;
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
 
 /**
  * @author Julious Igmen
@@ -23,7 +25,8 @@ public abstract class Android {
     @NonNull
     public abstract String id();
 
-    @NonNull public abstract String largeImageUrl();
+    @NonNull
+    public abstract String largeImageUrl();
 
     @NonNull
     public abstract String name();
@@ -31,10 +34,20 @@ public abstract class Android {
     @AutoValue.Builder
     public abstract static class Builder {
         public abstract Android build();
+
         public abstract Builder setAvatarUrl(String avatarUrl);
+
         public abstract Builder setBiography(String biography);
+
         public abstract Builder setId(String id);
+
         public abstract Builder setLargeImageUrl(String largeImageUrl);
+
         public abstract Builder setName(String name);
+    }
+
+    public static TypeAdapter<Android> typeAdapter(Gson gson) {
+        return new AutoValue_Android
+                .GsonTypeAdapter(gson);
     }
 }
